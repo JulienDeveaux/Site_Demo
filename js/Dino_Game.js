f371d20cd7b388abef57eeb8e642d7a377a3bd98
Julien_Deveaux <julien.deveaux@hotmail.com>
2021-05-02 20:48:20 +0200

    Various adds

--- js/Dino_Game.js
+++ js/Dino_Game.js
@@ -1,12 +1,12 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1250,
+  height: 800,
   physics: {
     default: 'arcade',
     arcade: {
       gravity: { y: 300 },
-      debug: true
+      debug: false
     }
   },
   scene: {
@@ -18,93 +18,168 @@ var config = {
 var game = new Phaser.Game(config);
 var input;
 var cactus;
-var dino;
+var car;
 var vitesse = 1;
 var posAvEcran;
+var tileSprite;
+var emiterSmoke1;
+var emiterSmoke2;
 
 function preload () {
   //this.load.setBaseURL('https://labs.phaser.io/assets');   // Append url avant les assets
 
+  this.load.image('skyO', 'img/cloudsOriginal.png');
   this.load.image('sky', 'img/clouds.png');
   this.load.image('logo', 'img/phaser3-logo.png');
   //this.load.image('particle', 'assets/particles/red.png');
-  this.load.image('particle', 'img/smoke-puff.png');
+  this.load.image('smoke1', 'img/smoke-puff.png');
+  this.load.image('smoke2', 'img/white-smoke.png');
   this.load.image('dino', 'img/dinotestwhite.png');
-  this.load.image('cactus', 'img/cactustest.png');
+  this.load.image('car', 'img/car.png');
+  this.load.image('cactus', 'img/cactus.png');
   this.load.image('reset', 'img/reset.png');
 }
 
 function create () {
+  this.cameras.main.setBackgroundColor('rgba(0, 255, 0, 0.5)');
   input = this.input;   // Récup souris
-  posAvEcran = this.cameras.main.centerX * 2 + 50;    // position écran un peu hors bordure à droite pour faire apparaitre le cactus
-  this.add.image(400, 300, 'sky');                    // Le fond
+  posAvEcran = this.cameras.main.centerX * 2 + 50;
+  this.add.image(500, 330, 'skyO').setScrollFactor(0);    // Static image
+  //tileSprite = this.add.tileSprite(600, 400, 1250, 800, 'skyO');   // Create a tilesprite (x, y, width, height, key)
+  /*tileSprite = this.make.tileSprite({
+    x: 0,
+    y: 250,
+    width: 5000,
+    height: 800,
+    key: 'skyO',
+    add: true
+  })
+  tileSprite.setScrollFactor(1, 0);*/
 
-  var particles = this.add.particles('particle');     // La fumée particule
+  var smoke1 = this.add.particles('smoke1');
+  var smoke2 = this.add.particles('smoke2');
 
-  var logo = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY - 200, 'logo');   // X up Y horizon Le logo phaser
+  var logo = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY - 200, 'logo').setScrollFactor(0);
 
-  cactus = this.physics.add.image(100, this.cameras.main.centerY * 2 - 50 , 'cactus');    // Le cactus
-  cactus.setCollideWorldBounds(true);                                                     // Collide avec les bordure de la fenêtre sinon il tombe
+  cactus = this.physics.add.image(100, this.cameras.main.centerY * 2 - 50 , 'cactus');
+  cactus.setCollideWorldBounds(true);
 
-  var emitter = particles.createEmitter({   // Sa c'est les particules de fumée
+  car = this.physics.add.image(200, 100, 'car');
+
+  emiterSmoke1 = smoke1.createEmitter({
+    acceleration: true,
+    accelerationX: Math.ceil((Math.random() - 0.5) * 2) < 1 ? -600 : -400, //-1000,
+    accelerationY: 0,
     speed: 100,
     scale: { start: 0.5, end: 0 },
-    blendMode: 'ADD'
+    angle: {min: -35, max: -90},
+    blendMode: 'ADD',
+    gravityY: 50,
+    frequency: 150,
+    rotate: { start: 0, end: 180 }
   });
 
-  dino = this.physics.add.image(200, 100, 'dino');    // Le dino
-
-  dino.setGravityY(100);    // Affecté par la gravité
-  dino.inputEnabled = true; // Controllable
-  dino.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));    //Rebondis sur l'axe Y
-  dino.setCollideWorldBounds(true);                       // Collide avec les bordures du monde
-
-  this.physics.add.collider(cactus, dino, function(cactus, dino) {    // Collision avec le cactus et le dino
-    cactus.x = posAvEcran;      // reset la position du cactus
-    vitesse += 0.5;             // Augmente la vitesse
+  emiterSmoke2 = smoke2.createEmitter({
+    acceleration: true,
+    accelerationX: Math.ceil((Math.random() - 0.5) * 2) < 1 ? -600 : -400, //-1000,
+    accelerationY: 0,
+    speed: 100,
+    scale: { start: 0.5, end: 0 },
+    angle: {min: -35, max: -90},
+    blendMode: 'ADD',
+    frequency: 100,
+    gravityY: 50,
+    rotate: { start: 0, end: 360 }
   });
 
-  this.buttonup = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY- 96, 'dino').setInteractive();   // Les boutons de controle du dino
+  car.setGravityY(100);
+  car.inputEnabled = true;
+  car.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  car.setCollideWorldBounds(true);
+
+  this.physics.add.collider(cactus, car, function(cactus, car) {
+    cactus.x = posAvEcran;
+    vitesse += 0.5;
+  });
+
+  this.buttonup = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY- 96, 'car').setInteractive().setScrollFactor(0);;
   this.buttonup.on('pointerover', function(){this.buttonup.setTint(0xff0000);}, this)
   this.buttonup.on('pointerout', function(){this.buttonup.setTint(0xffffff);}, this)
   this.buttonup.on('pointerdown', function(){
-    dino.body.velocity.y = dino.body.velocity.y - 400;
+    car.body.velocity.y = car.body.velocity.y - 400;
   });
   //------
-  this.buttondown = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY- 96 + 60, 'dino').setInteractive();
+  this.buttondown = this.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY- 96 + 60, 'car').setInteractive().setScrollFactor(0);;
   this.buttondown.on('pointerover', function(){this.buttondown.setTint(0x00ff00);}, this)
   this.buttondown.on('pointerout', function(){this.buttondown.setTint(0xffffff);}, this)
   this.buttondown.on('pointerdown', function(){
-    dino.body.velocity.y = dino.body.velocity.y + 400;
+    car.body.velocity.y = car.body.velocity.y + 400;
   });
   //------
-  this.buttondroite = this.add.sprite(this.cameras.main.centerX - 50, this.cameras.main.centerY- 96 + 30, 'dino').setInteractive();
+  this.buttondroite = this.add.sprite(this.cameras.main.centerX - 50, this.cameras.main.centerY- 96 + 30, 'car').setInteractive().setScrollFactor(0);;
   this.buttondroite.on('pointerover', function(){this.buttondroite.setTint(0xffff00);}, this)
   this.buttondroite.on('pointerout', function(){this.buttondroite.setTint(0xffffff);}, this)
   this.buttondroite.on('pointerdown', function(){
-    dino.body.velocity.x = dino.body.velocity.x - 200;
+    car.body.velocity.x = car.body.velocity.x - 200;
   });
   //------
-  this.buttongauche = this.add.sprite(this.cameras.main.centerX + 50, this.cameras.main.centerY- 96 + 30, 'dino').setInteractive();
+  this.buttongauche = this.add.sprite(this.cameras.main.centerX + 50, this.cameras.main.centerY- 96 + 30, 'car').setInteractive().setScrollFactor(0);;
   this.buttongauche.on('pointerover', function(){this.buttongauche.setTint(0xff0050);}, this)
   this.buttongauche.on('pointerout', function(){this.buttongauche.setTint(0xffffff);}, this)
   this.buttongauche.on('pointerdown', function(){
-    dino.body.velocity.x = dino.body.velocity.x + 200;
+    car.body.velocity.x = car.body.velocity.x + 200;
   });
 
-  this.reset = this.add.sprite(this.cameras.main.centerX - 350, this.cameras.main.centerY - 200, 'reset').setInteractive();  // Bouton reset de la vitesse
+  this.cursors = this.input.keyboard.createCursorKeys();      // input clavier
+
+  this.reset = this.add.sprite(this.cameras.main.centerX - 550, this.cameras.main.centerY - 200, 'reset').setInteractive().setScrollFactor(0);;
   this.reset.on('pointerdown', function(){
     vitesse = 1;
   });
 
-  emitter.startFollow(dino);      //Les particules sur le dino
+  this.cameras.main.startFollow(car).setFollowOffset(0, 250);   //suivi caméra
+  //this.cameras.main.focusOnXY(car.x, 20);
+
+  emiterSmoke1.startFollow(car);
+  emiterSmoke1.followOffset.set(-50, 0);
+  emiterSmoke2.startFollow(car);
+  emiterSmoke2.followOffset.set(-50, 0);
 }
 
 function update() {
-  if(cactus.x <= this.cameras.main.centerX / 10) {      // On fais avancer le cactus jusqu'au bord
+  //tileSprite.x -=8;
+  if(cactus.x <= this.cameras.main.centerX / 10) {
     cactus.x = this.cameras.main.centerX * 2 + 50;
     vitesse += 0.5;
   } else {
     cactus.x -= vitesse;
+  }
+
+  // Controls
+  if (this.cursors.left.isDown) {
+    car.body.velocity.x = car.body.velocity.x - 200;
+    emiterSmoke1.frequency = 150;
+    emiterSmoke2.frequency = 100;
+  }
+  else if(this.cursors.right.isDown) {
+    car.body.velocity.x = car.body.velocity.x + 200;
+    emiterSmoke1.frequency = 150;
+    emiterSmoke2.frequency = 100;
+  }
+  if(this.cursors.up.isDown) {
+    car.body.velocity.y = car.body.velocity.y - 400;
+    emiterSmoke1.frequency = 150;
+    emiterSmoke2.frequency = 100;
+  }
+  else if(this.cursors.down.isDown) {
+    car.body.velocity.y = car.body.velocity.y + 400;
+    emiterSmoke1.frequency = 150;
+    emiterSmoke2.frequency = 100;
+  }
+  if(this.cursors.space.isDown) {
+    car.body.velocity.x = 0;
+    car.body.velocity.y -= 0.5;
+    emiterSmoke1.frequency = 300;
+    emiterSmoke2.frequency = 500;
   }
 }
